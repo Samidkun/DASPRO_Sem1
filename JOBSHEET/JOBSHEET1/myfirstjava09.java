4511b4b3a0f1a71ecd7fdeeab9e009f8c4f35b53
Saya Samid(DIMAS ARYA) <147258313+Samidkun@users.noreply.github.com>
2023-11-26 13:54:26 +0700

    Add files via upload

---
+++ JOBSHEET/JOBSHEET1/myfirstjava09.java
@@ -0,0 +1,10 @@
+/**
+ * myfirstjava09
+ */
+public class myfirstjava09 {
+
+    public static void main(String[] args) {
+        System.out.println("my name is samid");
+        System.out.println("i am a new student in the departement of information techonolgy");
+    }
+}
