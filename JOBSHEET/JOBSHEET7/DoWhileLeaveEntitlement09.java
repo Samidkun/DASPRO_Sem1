d44534fd3aa713c0dad6f4365bd0a4c8889b74d2
Saya Samid(DIMAS ARYA) <147258313+Samidkun@users.noreply.github.com>
2023-11-26 15:28:04 +0700

    Add files via upload

---
+++ JOBSHEET/JOBSHEET7/DoWhileLeaveEntitlement09.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DoWhileLeaveEntitlement09 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int leaveEntitlement, numLeave;
+        String confirmation;
+        int jatahCuti, jumlahHari;
+        int konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = input.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            confirmation = input.next();
+
+            if (confirmation.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = input.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    break;
+                }
+            } else {
+                break;
+            }
+        } while (jatahCuti > 0);
+
+        input.close(); // Close the Scanner when you're done using it
+    }
+}
---
+++ JOBSHEET/JOBSHEET7/WhileOvertimePay09.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * WhileOvertimePay09
+ */
+public class WhileOvertimePay09 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numEmployee = 0;
+        int overtimeHours = 0;
+        double overtimePay = 0.0;
+        double totalOvertimePay = 0.0;
+        
+
+         
+    }
+}
