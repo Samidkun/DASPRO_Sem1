06e94a93f77017231855158e743d6c2d10caa327
Saya Samid(DIMAS ARYA) <147258313+Samidkun@users.noreply.github.com>
2023-11-26 14:29:01 +0700

    Add files via upload

---
+++ JOBSHEET/JOBSHEET5/selection109.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+/**
+ * selection109
+ */
+public class selection109 {
+
+    public static void main(String[] args) {
+          Scanner input09 = new Scanner(System.in);
+System.out.println("Input a number = ");
+int number = input09.nextInt();
+if(number%2 == 0){
+        System.out.println(number+"is an even number!");
+}else{
+    System.out.println(number+"is an odd number!");
+}
+    
+    
+    }
+
+}
