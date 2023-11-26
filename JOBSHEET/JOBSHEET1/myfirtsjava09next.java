e427d45ea24377a286a9f7606fc4c094c6f09725
Saya Samid(DIMAS ARYA) <147258313+Samidkun@users.noreply.github.com>
2023-11-26 14:00:41 +0700

    Add files via upload

---
+++ JOBSHEET/JOBSHEET1/myfirtsjava09next.java
@@ -0,0 +1,14 @@
+/**
+ * myfirtsjava09next
+ */
+public class myfirtsjava09next {
+
+    public static void main(String[] args) {
+        System.out.println( "***STUDENT IDENTITY***");
+        System.out.println( "Name : SAMID");
+        System.out.println( "ID NUMBER : 09");
+        System.out.println( "Class : SIB 1G");
+        System.out.println( "Study program : business information systems");
+        System.out.println( "Departement : Technology information");
+    }
+}
